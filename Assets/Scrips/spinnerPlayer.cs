90526e7b1ad7ab52527a682fb600940ec221360a
taichuan2002 <134790352+taichuan2002@users.noreply.github.com>
2023-09-02 21:15:27 +0700

    Revert "no message"
    This reverts commit 7a5dfee67872216b5da3d58b24a03347e7e942e0.

--- Assets/Scrips/spinnerPlayer.cs
+++ Assets/Scrips/spinnerPlayer.cs
@@ -13,7 +13,6 @@ public class SpinnerPlayer : MonoBehaviour
 {
     [SerializeField] GameObject _panelCanel;
     [SerializeField] GameObject _panelMoney;
-    [SerializeField] GameObject _panelUnLockLevel;
     [SerializeField] ScrollRect scrollRect;
     [SerializeField] CircularScrollingList _list;
     public TextMeshProUGUI txtName;
@@ -345,7 +344,6 @@ public class SpinnerPlayer : MonoBehaviour
                 LevelSSJ[center] = levelValue;
                 OnInitCoin();
                 FindObjectOfType<HomeUI>().UpdateCoin(Coin);
-                _panelUnLockLevel.SetActive(true);
             }
             else
             {
@@ -365,7 +363,6 @@ public class SpinnerPlayer : MonoBehaviour
     public void Canel()
     {
         _panelCanel.SetActive(false);
-        _panelUnLockLevel.SetActive(false);
     }
 
     public void ResetListPlayer()
